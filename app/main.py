0950ee3ffa3d4f1d888b58b16f4a19f52d004cea
Nure Siddiq <nalamsiddiq@gmail.com>
2026-02-01 05:32:56 +0600

    Add files

--- app/main.py
+++ app/main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.v1.api import api_router
 import logging
+import time
 
-# Log to the volume shared with Filebeat
+# 1. Setup Logging
 logging.basicConfig(
     filename='/var/log/app/fastapi.log',
     level=logging.INFO,
     format='%(asctime)s | %(levelname)s | %(message)s',
     datefmt='%Y-%m-%dT%H:%M:%S'
 )
+logger = logging.getLogger("api")
 
 app = FastAPI(title="AWS ELK Lab")
+
+# 2. Add Middleware to log every request
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    
+    response = await call_next(request)
+    
+    duration = round(time.time() - start_time, 4)
+    
+    # This line is what actually WRITES to the file
+    logger.info(f"{request.method} | {request.url.path} | {response.status_code} | {duration}")
+    
+    return response
+
 app.include_router(api_router, prefix="/api/v1")
